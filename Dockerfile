46f4c01dae19f2b651cc7ac5884c5888ac11d435
Claude Manchester <cloudeal@hotmail.com>
2022-05-13 18:02:04 +0800

    update python.sh&Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM centos:7
-ENV LD_LIBRARY_PATH="/srv/www/data/app/DOIM/Server/bin/python/lib/:/srv/www/data/app/DOIM/Server/python/:/srv/www/data/app/DOIM/Server/webexpress/:/srv/www/data/app/DOIM/Server/bin/"
-WORKDIR /
-COPY /data .
+ENV LD_LIBRARY_PATH="/srv/www/libs/"
 WORKDIR /srv/www
 COPY . .
-CMD ["/srv/www/data/app/DOIM/Server/python/python.sh", "app.py"]
+CMD ["/srv/www/python/python.sh", "/srv/www/server/app.py"]
 
--- python/python.sh
+++ python/python.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/bash
 
-export PYTHONPATH="/srv/www/data/app/DOIM/Server/python/Lib"
-export PYTHONPATH="/srv/www/data/app/DOIM/Server/python/DLLs:$PYTHONPATH"
-export PYTHONPATH="/srv/www/data/app/DOIM/Server/python/Lib/site-packages:$PYTHONPATH"
-export PYTHONPATH="/srv/www/data/app/DOIM/Server/webexpress/python:$PYTHONPATH"
-echo $PYTHONPATH
-export PYTHONHOME="/srv/www/data/app/DOIM/Server/webexpress/python"
-#export PYTHONPATH="/data/app/DOIM/Server/webexpress/python/lib/python3.7/site-packages:$PYTHONPATH"
+export PYTHONPATH="/srv/www/py_dependencies"
+export PYTHONHOME="/srv/www/python"
 
-echo $1 $2 $3 $4 $5
-#/usr/bin/python3 $1 $2 $3 $4 $5 $6 $7 $8
-/srv/www/data/app/DOIM/Server/python/bin/python3 $1 $2 $3 $4 $5 $6 $7 $8
+/srv/www/python/bin/python3 $1 $2 $3 $4 $5 $6 $7 $8
 
