c8ce41a66450bc3190a386c3d6a608e23200bdde
Claude Manchester <cloudeal@hotmail.com>
2022-05-16 12:48:05 +0800

    Fix PYTHONPATH

--- python/python.sh
+++ python/python.sh
@@ -3,7 +3,7 @@
 export PYTHONPATH="/srv/www/python/Lib"
 export PYTHONPATH="/srv/www/python/DLLs:$PYTHONPATH"
 export PYTHONPATH="/srv/www/python/Lib/site-packages:$PYTHONPATH"
-export PYTHONPATH="/srv/www/py_dependencies"
+export PYTHONPATH="/srv/www/py_dependencies:$PYTHONPATH"
 export PYTHONHOME="/srv/www/python"
 
 /srv/www/python/bin/python3 $1 $2 $3 $4 $5 $6 $7 $8
